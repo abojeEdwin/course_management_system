f33cc832a2b54dba3984937fc1134fa412e4bc94
Edwin Aboje <abojeedwin@gmail.com>
2025-03-06 03:18:43 +0100

    merged issues resolved

--- test/course_management_system/test_courseclass.py
+++ test/course_management_system/test_courseclass.py
@@ -2,8 +2,8 @@ import unittest
 from unittest import TestCase
 
 
-from courseManagementSystem.src.course_management_system.course import Course
-from courseManagementSystem.src.course_management_system.student import Student
+from src.course_management_system.course import Course
+from src.course_management_system.student import Student
 
 class TestCourse(TestCase):
     def setUp(self):
